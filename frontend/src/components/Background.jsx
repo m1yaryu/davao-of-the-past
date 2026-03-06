d09ff110393aa979b39b402d2ba2d56787681b98
5000adel <xsrillixon@gmail.com>
2026-03-06 19:56:00 +0800

    feat: added background design

---
+++ frontend/src/components/Background.jsx
@@ -0,0 +1,17 @@
+import './TopBar'
+import TopBar from './TopBar'
+import BottomBar from './BottomBar'
+import TestCard from './TestCard'
+import './Background.css'
+import { useState } from 'react'
+
+function Background() {
+    return (
+        <div className="home">
+            <div class="glow glow-1"></div>
+            <div class="glow glow-2"></div>
+        </div>
+    )
+}
+
+export default Background
